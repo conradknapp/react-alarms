6462557bdd1b1fb626f2d3d3525218785ade506d
Conrad Knapp <knappco@bc.edu>
2018-05-11 19:18:03 -0400

    Removes changes root route on server to simply return index.html file

--- server.js
+++ server.js
@@ -17,10 +17,6 @@ mongoose
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
 app.use('/api/alarms', alarmRoutes);
 app.use('/api/logs', logRoutes);
 
